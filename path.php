593b989dd21c8f5619eeee130b80fe0cd4eb4924
armelle23 <armelle.riviere@efrei.net>
2017-05-03 23:01:19 +0200

    Add files via upload

---
+++ path.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Autoroutes</title>
+    <link href="assets/lib/css/bootstrap.min.css" rel="stylesheet">
+    <link href="menu_deroulant.css" rel="stylesheet">
+    <link href="bibou.css" rel="stylesheet">
+    <link href="login.css" rel="stylesheet">
+       <link href="path.css" rel="stylesheet">
+ 
+
+
+</head>
+<body>
+
+<div id="container">
+
+    <div id="login">
+
+        <div class="content">
+            <form method="post" action="index.html">
+
+
+                <div class="form">
+                    <label for="username">Ville de départ : Paris</label>
+                    
+                </div>
+
+                <div class="form">
+                    <label for="password">Ville d'arrivée : Lyon</label>
+                   
+                </div>
+
+             </div>
+
+
+    </div>
+
+    <div id="container">
+
+    <div id="login">
+
+        <div class="content">
+            
+                <div class="form">
+                    <label for="username">Trajet</label>
+                    
+                </div>
+
+               
+
+            
+        </div>
+        
+
+    </div>
+
+
+
+
+        
+
+      <button type="submit" <a href="accueil.html">Modifier </a> </button>
+
+
+    
+
+
+   
+</body>
+</html>
+
