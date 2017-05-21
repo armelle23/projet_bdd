39ffc72433ee20a82588d90c68ab364e47636f67
melanie roude <melanie roude>
2017-05-21 13:16:47 +0200

    Calcul ville non fonctionnel

--- path.php
+++ path.php
@@ -7,6 +7,9 @@
     {
         die('Erreur : '.$e->getMessage());
     }
+
+    $vDepart= $_POST['vDepart'];
+    $varrivee= $_POST['varrivee'];
 ?>
 
 
@@ -29,31 +32,47 @@
 
 </head>
 <body>
-    <form method="post" action="BDD_project.php">
-        <div id="container">
-            <div id="login">
-                    <strong>Ville de depart </strong> <?php echo $_POST['vdepart'];  ?> 
-            </div>
-            <div class="content">   
-        
-            </div>
+        <div id="login">
+            <strong>Ville de depart </strong> <?php echo $vDepart;  ?> 
         </div>
-        <div id="container">
-            <div id="login">
-               <strong>Ville d'arrivée</strong> <?php echo $_POST['varrivee'];  ?>
-            </div>
-            <div class="content">   
-        
-            </div>
-        </div>
-        
 
-     <button type="submit" <a >Valider </a> </button>
+        <?php
+            $codePostalDepart = $bdd->prepare("SELECT CodeP FROM ville WHERE NomV = '$vDepart'"); 
+            $codePostalDepart->execute();
+            $codeDepart = $codePostalDepart->fetchAll()[0]['CodeP'];
 
+            print_r($codeDepart);
 
+            $KmDepart = $bdd->prepare("SELECT t.DuKm FROM  ville v, sortie s, troncon t  WHERE s.CodeP = '$codeDepart' AND s.CodT = t.CodT"); 
+            $KmDepart->execute();
+            $kmDebut = $KmDepart->fetchAll()[0]['DuKm'];
 
+            print_r($kmDebut);
 
-   
+          $codePostalArrive =  $bdd->prepare("SELECT CodeP FROM ville WHERE NomV = '$varrivee'"); 
+            $codePostalArrive->execute();
+            $codeArrive = $codePostalArrive->fetchAll()[0]['CodeP'];
+
+            print_r($codeArrive);
+
+            $KmFin = $bdd->prepare("SELECT t.AuKm FROM  ville v, sortie s, troncon t  WHERE s.CodeP = '$codeArrive' AND s.CodT = t.CodT "); // CODE A A AJOUTER
+            $KmFin->execute();
+            $KmArrive = $KmFin->fetchAll()[0]['AuKm'];
+
+            print_r($KmArrive);
+
+            $troncons = $bdd->prepare("SELECT CodT FROM troncon WHERE DuKm >= '$kmDebut' AND AuKm <= '$KmArrive' ORDER BY CodT ASC");
+            $troncons->execute();
+
+            $trajet = $troncons->fetchAll();
+            print_r($trajet);
+
+            $nbKm = $KmArrive- $kmDebut;
+            echo "Nombre de km parcouru entre les 2 villes : ".$nbKm;
+        ?> 
+        <div id="login">
+           <strong>Ville d'arrivée</strong> <?php echo $varrivee;  ?>
+        </div>  
 </body>
 </html>
 
